5d4fb01e1c328d45fc63e40a04cb6ba4a2a94985
EA-Cortes <33381916+EA-Cortes@users.noreply.github.com>
2018-07-08 00:55:34 -0400

    Add files via upload
    AVL Tree
    Working Functions: Insert(), Search()
    Searches by int key, outputs taskName if key is found

--- assignment4.c
+++ assignment4.c
@@ -6,7 +6,7 @@ void createTask();
 void getTask();
 
 // AVL Tree node
-struct Node{
+typedef struct Node{
 	int key;
 	char* taskName;
 
@@ -16,58 +16,112 @@ struct Node{
 
 }Node;
 
+int height(Node *node);
+int getBal(Node *node);
 int max(int a, int b);
-int height(struct Node *node);
-int getBal(struct Node *node);
-struct Node *newNode(int key);
-struct Node *rightRotate(struct Node *y);
-struct Node *leftRotate(struct Node *x);
-struct Node *insert(struct Node *node, int key);
-void preOrder(struct Node *node);
-void inOrder(struct Node *node);
-struct Node * searchTree(struct Node *node, int key);
+Node *insert(Node *node, int key, char *string);
+Node *newNode(int key, char *string);
+Node * search(Node *node, int key);
+Node *rightRotate(Node *y);
+Node *leftRotate(Node *x);
+void getInputs(int n, Node *node);
+void inOrder(Node *node);
 
 int main(){
 	int n = 0;
-	struct Node *root = NULL;
-	root = insert(root, 10);
-  // root = insert(root, 10);
-  root = insert(root, 20);
-  root = insert(root, 30);
-  root = insert(root, 40);
-  root = insert(root, 50);
-  root = insert(root, 30);
-  root = insert(root, 25);
+	Node *root = NULL;
+	scanf("%d", &n);
+  getInputs(n, root);
 
-  
-
-  searchTree(root, 40);
-  
-  printf("In order traversal of the tree above is\n");
+  printf("\nIn Order Traversal of values in tree:\n");
   inOrder(root);
-  printf("\n");
-	// scanf("%d", n);
-	// n will be the following number of lines in which the user will choose to either add/call tasks
-		// Input format
-		// int 1 (createTask) OR 2 (getTask)
-			// 1 -- char[19] taskName -- int priority
-			// 2 -- int
 }
 
 // Create new node, malloc its memory, init its values
-struct Node *newNode(int key){
-  struct Node *node = (struct Node*)malloc(sizeof(struct Node));
+Node *newNode(int key, char *string){
+  Node *node = (Node*)malloc(sizeof(Node));
   node->key = key;
+  node->taskName = string;
   node->left = NULL;
   node->right = NULL;
   node->height = 1;
   return node;
 }
 
+
+// Function that inserts and balances new nodes simultaneously
+// Node Parameters: 19 char string, int priority
+Node *insert(Node *node, int key, char *string){
+  // If node is empty, successfully add
+  if(node == NULL){    
+    printf("ADDED\n");
+    return newNode(key, string);
+
+  }
+  // If node already exists
+  if(key == node->key){
+    printf("REDUNDANT\n");
+  }
+
+  if(key < node->key){
+    node->left = insert(node->left, key, string);
+  }else if(key > node->key){
+    node->right = insert(node->right, key, string);
+  }else{
+    return node;
+  }
+
+  // Update height
+    node->height = 1 + max(height(node->left), height(node->right));
+
+  // deal with unbalanced tree cases
+    int balance = getBal(node);
+
+    // LL Rotate
+    if(balance > 1 && key < node->left->key) return rightRotate(node);
+    
+    // RR Rotate
+    if(balance < -1 && key > node->right->key) return leftRotate(node);
+    
+    // LR Rotate
+    if(balance > 1 && key > node->left->key){
+      node->left = leftRotate(node->left);
+      return rightRotate(node);
+    }
+    
+    // RL Rotate
+    if(balance < -1 && key < node->right->key){
+      node->right = rightRotate(node->right);
+      return leftRotate(node);
+    }
+
+    return node;
+}
+
+Node *search(Node *node, int key){
+  if(node == NULL){
+    printf("NON-EXISTANT\n");
+    return node;
+  }else if(node->key == key){
+    if(node->taskName != NULL){
+      printf("%s\n", node->taskName);
+    }
+  }
+
+
+  if(key < node->key){
+      return search(node->left, key);
+  }else if(key > node->key){
+     return search(node->right, key);
+  }
+  return node;
+}
+
+
 // Utility function that rotates to the right
-struct Node *rightRotate(struct Node *y){
-  struct Node *x = y->left;
-  struct Node *T2 = x->right;
+Node *rightRotate(Node *y){
+  Node *x = y->left;
+  Node *T2 = x->right;
 
   x->right = y;
   y->left = T2;
@@ -79,9 +133,9 @@ struct Node *rightRotate(struct Node *y){
 }
 
 // Utility function that rotates to the left
-struct Node *leftRotate(struct Node *x){
-  struct Node *y = x->right;
-  struct Node *T2 = y->left;
+Node *leftRotate(Node *x){
+  Node *y = x->right;
+  Node *T2 = y->left;
 
   y->left = x;
   x->right = T2;
@@ -92,107 +146,25 @@ struct Node *leftRotate(struct Node *x){
   return y;
 }
 
-// Function that inserts and balances new nodes simultaneously
-struct Node *insert(struct Node *node, int key){
-
-  // BST Insertion
-  if(node == NULL){
-    return newNode(key);
-  }
-
-  if(key == node->key){
-    printf("%d is REDUNDANT\n", node->key);
-  }
-
-  if(key < node->key){
-    node->left = insert(node->left, key);
-  }else if(key > node->key){
-    node->right = insert(node->right, key);
-  }else{
-    return node;
-  }
-
-  // Update height
-  node->height = 1 + max(height(node->left), height(node->right));
-
-  // check if tree is balanced;
-  int balance = getBal(node);
-
-  // deal with unbalanced tree cases
-  // Left Left Case
-  if(balance > 1 && key < node->left->key){
-    return rightRotate(node);
-  }
-
-  // Right Right case
-  if(balance < -1 && key > node->right->key){
-    return leftRotate(node);
-  }
-  
-  // Left Right case
-  if(balance > 1 && key > node->left->key){
-    node->left = leftRotate(node->left);
-    return rightRotate(node);
-  }
-
-  // Right Left case
-  if(balance < -1 && key < node->right->key){
-    node->right = rightRotate(node->right);
-    return leftRotate(node);
-  }
-
-  // If tree is already balanced
-  return node;
-}
-
-struct Node *searchTree(struct Node *node, int key){
-  if(node == NULL){
-    printf("Value not found.\n");
-    return node;
-  }else if(node->key == key){
-    printf("%d found in tree!\n", key);
-  }
-
-
-  if(key < node->key){
-      return searchTree(node->left, key);
-  }else if(key > node->key){
-     return searchTree(node->right, key);
-  }
-  return node;
-}
-
 // Utility function to see which of two values is greater
 int max(int a, int b){
   return (a > b) ? a : b;
 }
 
-int height(struct Node *node){
-  if (node == NULL){
-    return 0;
-  }
-  return node->height;
+int height(Node *node){
+  return (node == NULL) ? 0 : node->height;
 }
 
 // Utility function to get the balance favtor of a Node
-int getBal(struct Node *node){
+int getBal(Node *node){
   if(node == NULL){
     return 0;
   }
   return height(node->left) - height(node->right);
 }
 
-// Utility function to print preorder traversal
-void preOrder(struct Node *node){
-  if(node !=NULL){
-    printf("%d ", node->key);
-    preOrder(node->left);
-    preOrder(node->right);
-  }
-}
-
 // Utility function to print in-order traversal
-void inOrder(struct Node *node){
+void inOrder(Node *node){
   if(node != NULL){
     inOrder(node->left);
     printf("%d ", node->key);
@@ -200,11 +172,25 @@ void inOrder(struct Node *node){
   }
 }
 
-// Create Task and assign it a priority
-void createTask(){
 
-}
+void getInputs(int n, Node *node){
+  int i = 0, state = 0, tempKey = 0;
+  if(n == 0){
+    printf("No input\n");
+  }else{
+    for(i = 0; i < n; i++){
+      scanf("%d", &state);
 
-void getTask(){
-
+      if(state == 1){
+        char *tempName = (char *)malloc(sizeof(char) * 20);
+        scanf("%s %d", tempName, &tempKey);
+        node = insert(node, tempKey, tempName);
+        free(tempName);
+        tempName = NULL;
+      }else if(state == 2){
+        scanf("%d", &tempKey);
+        search(node, tempKey);        
+      }
+    }
+  }
 }
